54d79545f5cefd012578f9a9f16ba581b426e68f
brysonbest <brycekalden@gmail.com>
2022-04-14 19:02:54 -0700

    Add admin dashboard counts.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,5 +6,22 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: user, password: pass
   
   def show
+    @category = Category.find_each()
+    @categories = []
+    @category.each do |each|
+      products = each.products.order(created_at: :desc)
+      count = 0
+      products.each do |prod|
+        count += prod.quantity
+      end
+      cat = [each.name, count]
+      @categories.push(cat)
+    end
+    @product_totals = []
+    @category.each do |items|
+      products = items.products.order(created_at: :desc)
+      @product_totals.push(products)
+    end
+
   end
 end
